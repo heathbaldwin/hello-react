df7943d0320187079e06dd9812edad2df98e471f
heathbaldwin <105754991+heathbaldwin@users.noreply.github.com>
2023-03-27 21:45:43 -0400

    Magic Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
 
+function MagicButton () {
+  return (
+    <button>Magic</button>
+  );
+}
+
 export default function App() {
   return (
     <div>
+     
       <h1>Welcome to my React Project</h1>
       <p>My name is Heath</p>
+
+      <MagicButton/>
+
     </div>
   );
 }
